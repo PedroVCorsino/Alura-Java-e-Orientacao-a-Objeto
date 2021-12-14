62ee5cc21342042cfc85593248e49678b2ad39f0
Pedro Vinicius <pedro.santos@metasix.com.br>
2021-12-14 13:47:52 -0300

    Java Exceções: Aula03-Lançando exceções.

--- java-pilha/src/Conta.java
+++ java-pilha/src/Conta.java
@@ -1,4 +1,15 @@
 
 public class Conta {
+	double saldo;
+	
 	void deposita() {}
+
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
