b6e0c07c08f9510213735c37bec12d5ed22ec448
Pedro Vinicius <pedro.santos@metasix.com.br>
2021-12-20 17:57:43 -0300

    Java Collections: Aula04 - Mais praticas com relacionamentos.

--- gerenciador-de-cursos/src/br/com/alura/modelo/Curso.java
+++ gerenciador-de-cursos/src/br/com/alura/modelo/Curso.java
@@ -31,4 +31,18 @@ public class Curso {
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
 	}
+	
+	public int getTempoTotal() {
+		int tempoTotal = 0;
+		for (Aula aula : aulas) {
+			tempoTotal += aula.getTempo();
+		}
+		return tempoTotal;
+	}
+	
+	@Override
+	public String toString() {
+		return "Curso: " + nome + ", tempo total: " + this.getTempoTotal() + ", Aulas: " + this.aulas + "]";
+	}
+	
 }
---
+++ gerenciador-de-cursos/src/br/com/alura/teste/TestandoCurso2.java
@@ -0,0 +1,33 @@
+package br.com.alura.teste;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import br.com.alura.modelo.Aula;
+import br.com.alura.modelo.Curso;
+
+public class TestandoCurso2 {
+
+	public static void main(String[] args) {
+		Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
+
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
+		
+		List<Aula> aulasImutaveis = javaColecoes.getAulas();
+		System.out.println(aulasImutaveis);
+		
+		List<Aula> aulas = new ArrayList<Aula>(aulasImutaveis); 
+		
+		Collections.sort(aulas);
+		System.out.println(aulas);
+		System.out.println(javaColecoes.getTempoTotal());
+		
+		System.out.println(javaColecoes.toString());
+		
+		
+	}
+
+}
